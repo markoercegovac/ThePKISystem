4e0d9665d0ae39dd6d4f383a6398984e0ce29529
majakesin <kesin.maja@gmail.com>
2020-04-04 15:29:58 +0200

    Fixed lombok

--- Backend/src/main/java/com/pki/app/controller/CertificateController.java
+++ Backend/src/main/java/com/pki/app/controller/CertificateController.java
@@ -1,6 +1,7 @@
 package com.pki.app.controller;
 
 import com.pki.app.model.SubjectData;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
